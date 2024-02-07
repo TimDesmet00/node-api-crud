da01deb387550d1d6669f974011897911e0bcfd6
tim <timdesmet00@gmail.com>
2024-02-07 13:21:35 +0100

    debut ajout token et routes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,2 +1,29 @@
 const jwt = require("jsonwebtoken");
 
+const generateToken = (userId) => {
+    const token = jwt.sign({ id: userId }, 'votre_clé_secrète', { expiresIn: '1h' });
+    return token;
+}
+
+const verifyToken = (token) => {
+    try {
+        const decoded = jwt.verify(token, 'votre_clé_secrète');
+        return decoded;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+module.exports.signup = async (req, res) => {
+    const userId = 'id_de_l_utilisateur';
+    const token = generateToken(userId);
+    res.json({ token });
+}
+
+module.exports.login = async (req, res) => {
+    const userId = 'id_de_l_utilisateur';
+    const token = generateToken(userId);
+    res.json({ token });
+}
+
